2d0fad631043c8f6a6905bccbd3c47737f3135d0
Filip Claesson <Filip@Filips-MacBook-Air.local>
2017-08-15 19:55:22 +0200

    small fix

--- public/controllers/csvReaderController.js
+++ public/controllers/csvReaderController.js
@@ -33,19 +33,7 @@ $scope.read_csv = function(){
 		//error handling
 		if (response.success){
 			console.log(response)
-			// var tripList = response.data.tripList
-			// console.log(response.data)
-			// allSubTrips = []
-			// $scope.Trips = tripList
-			
-			// for (var i in tripList){
-			// 	for (var j in tripList[i].subTrips){
-			// 		allSubTrips.push(tripList[i].subTrips[j])
-			// 	}
-			// }
-			
-			// $scope.subTrips = allSubTrips
-			// $scope.positionVariables = response.data.variables
+
 
 		}else{
 			console.log(response)
